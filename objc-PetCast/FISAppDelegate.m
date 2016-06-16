a066989afdda0d3da80fb88d00e841284de914f4
BettinaP <bettinaprophete@gmail.com>
2016-06-16 13:33:14 -0400

    done

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,35 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+
+    
+    //    FISDog *dogCanWoof =[[FISDog alloc] init];
+    //    NSLog(@"%@", [dogCanWoof makeASound]);
+    //    NSLog(@"%@", [dogCanWoof assaultTheMailman]);
+
+   
+    FISDog *newDog =[[FISDog alloc ]init];
+    FISPet *petAsDog = (FISPet *) newDog;
+    NSLog(@"%@", [petAsDog makeASound]);
+    
+    FISPet *newPet = [[FISPet alloc]init];
+    FISDog *dogAsPetCanWoof =(FISDog *) newPet;
+    NSLog(@"%@",[dogAsPetCanWoof makeASound]);
+    //NSLog(@"%@",[dogAsPetCanWoof assaultTheMailman]);
+
+   
+    
+    FISPet *secondDogIsAPet =[[FISDog alloc] init];
+    NSLog(@"%@", [secondDogIsAPet makeASound]);
+    NSLog(@"%@", [((FISDog *)secondDogIsAPet) assaultTheMailman]);
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISDog.h
@@ -0,0 +1,18 @@
+//
+//  FISDog.h
+//  objc-PetCast
+//
+//  Created by Bettina on 6/16/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@interface FISDog : FISPet
+
+
+-(NSString *)makeASound;
+
+-(NSString *)assaultTheMailman;
+
+@end
